75437fe12001c40eca5cd10bc08a71c8b660f071
pixis <hackndo@gmail.com>
2020-01-22 10:02:04 +0100

    improve imports

--- lsassy/__init__.py
+++ lsassy/__init__.py
@@ -5,5 +5,12 @@
 #  https://en.hackndo.com [EN]
 
 from .core import Lsassy
+from .modules.dumper import Dumper
+from .modules.logger import Logger
+from .modules.parser import Parser
+from .modules.writer import Writer
+
+__all__ = ["Lsassy", "Dumper", "Logger", "Parser", "Writer"]
 
 name = "lsassy"
+
