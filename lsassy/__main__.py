9c2fb9f51ae20709ff2a4e246334b241c359e76f
pixis <hackndo@gmail.com>
2020-06-23 14:47:21 +0200

    Remove useless files

--- lsassy/__main__.py
+++
@@ -1,6 +0,0 @@
-from lsassy.app import Lsassy
-import logging
-
-if __name__ == '__main__':
-    Lsassy(level=logging.DEBUG).get_dump_file(None, None, "procdump", no_powershell=True, exec_methods=("wmi", "task"),
-                                              procdump_path="C:\\Users\\rben\\Desktop\\Sysinternals\\procdump.exe")
