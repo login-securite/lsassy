50fe13c9a57cf9edecc0e2783cab8015bbdb23b7
Pixis <hackndo@gmail.com>
2021-03-19 15:31:12 +0100

    Update pypykatz dependency version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         'impacket',
         'netaddr',
-        'pypykatz>=0.3.0'
+        'pypykatz>=0.4.3'
     ],
     python_requires='>=3.6',
     classifiers=(
