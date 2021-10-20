e6fdd3ff74e4cdb07ef33db56f4cecd52f6e0245
pixis <hackndo@gmail.com>
2021-10-20 13:28:24 +0200

    Update to v3.0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="lsassy",
-    version="3.0.2",
+    version="3.0.3",
     author="Pixis",
     author_email="hackndo@gmail.com",
     description="Python library to extract credentials from lsass remotely",
--- tests/test_lsassy.py
+++ tests/test_lsassy.py
@@ -2,4 +2,4 @@ from lsassy import __version__
 
 
 def test_version():
-    assert __version__ == '3.0.2'
+    assert __version__ == '3.0.3'
