3c23022af2cffd3d2ec23547a900b275a72a3478
pixis <hackndo@gmail.com>
2022-05-31 20:49:55 +0200

    Fix #68

--- lsassy/dumpmethod/edrsandblast.py
+++ lsassy/dumpmethod/edrsandblast.py
@@ -41,7 +41,7 @@ class DumpMethod(IDumpMethod):
         self.clean_dependencies([self.edrsandblast, self.RTCore64, self.ntoskrnl])
 
     def get_commands(self, dump_path=None, dump_name=None, no_powershell=False):
-        cmd_command = """{} dump --kernelmode --driver {} --nt-offsets {} -o {}{}""".format(
+        cmd_command = """{} dump --usermode --kernelmode --driver {} --nt-offsets {} -o {}{}""".format(
             self.edrsandblast.get_remote_path(),
             self.RTCore64.get_remote_path(),
             self.ntoskrnl.get_remote_path(),
