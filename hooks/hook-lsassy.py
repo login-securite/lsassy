ecd5fdaff388c14b65d6c21792e258c739652547
pixis <hackndo@gmail.com>
2022-06-30 19:08:41 +0200

    Add automatic builds on push/PR on main

--- hooks/hook-lsassy.py
+++ hooks/hook-lsassy.py
@@ -4,7 +4,6 @@ from PyInstaller.utils.hooks import collect_submodules
 Note that output "table_output" has been removed. There's a bug regarding pyinstaller and 
 rich library which prevents compiling lsassy if this lib is somehow imported.
 """
-hiddenimports = (collect_submodules('lsassy.exec')
-                 + collect_submodules('lsassy.output')
-                 + collect_submodules('lsassy.dumpmethod'))
+hiddenimports = (collect_submodules('lsassy.exec') + collect_submodules('lsassy.output') + collect_submodules('lsassy.dumpmethod'))
 hiddenimports.remove('lsassy.output.table_output')
+hiddenimports.append('unicrypto.backends.pycryptodomex')
