ba01cf634aaea106a297e953039b5dace087df29
pixis <hackndo@gmail.com>
2024-01-24 14:04:05 +0100

    Fix tests bis

--- tests/test_lsassy.py
+++ tests/test_lsassy.py
@@ -199,7 +199,7 @@ class TestWriter(unittest.TestCase):
         file = dumper.dump(exec_methods=["smb"])
         self.assertIsNotNone(file)
 
-        credentials, tickets, masterkeys = Parser(file).parse()
+        credentials, tickets, masterkeys = Parser(HOSTNAME, file).parse()
         file.close()
         self.assertTrue(len(credentials) > 0)
 
