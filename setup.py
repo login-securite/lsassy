d3e04e3ad48d92e1fdc3089b5e0bf8d836bc27cd
pixis <hackndo@gmail.com>
2024-12-24 13:46:22 +0100

    Python version update

--- setup.py
+++ setup.py
@@ -27,10 +27,8 @@ setup(
     install_requires=["impacket", "netaddr", "pypykatz>=0.6.3", "rich"],
     python_requires=">=3.6",
     classifiers=(
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
